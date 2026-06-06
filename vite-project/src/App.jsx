e050ce0337e73a87a494bfeb142256565e3268d5
Abdulilah-Abdulrahman <abodehtml11@gmail.com>
2026-06-06 03:11:35 -0400

    تعديل الصفحه الرءسيه

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,39 +1,9 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [error , setError]=useState()
-   useEffect(()=>{
-   axios
-     .get(" https://jsonplaceholder.typicode.com/posts")
-     .then((response) => {
-      setPosts(response.data);
-     })
-
-     .catch((err) => {
-       setError("Failed to fetch data");
-      
-     });
-  
-   },[])
-   
-    if (error) return <h1>{error}</h1>;
   return (
-    
-    <div>
-  
-      <h1>HELLO</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>hello Abdulilah 2001</div>
+  )
 }
-
-
-
 
 export default App
